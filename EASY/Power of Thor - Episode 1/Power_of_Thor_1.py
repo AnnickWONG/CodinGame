8710471e5718f0b9d3c5a5253017cb59ef475b9b
Yuqi HUANG <49431901+AnnickWONG@users.noreply.github.com>
2022-04-08 12:26:05 +0200

    Update Power_of_Thor_1.py

--- EASY/Power of Thor - Episode 1/Power_of_Thor_1.py
+++ EASY/Power of Thor - Episode 1/Power_of_Thor_1.py
@@ -1,35 +1,23 @@
-# light_x: the X position of the light of power
-# light_y: the Y position of the light of power
-# initial_tx: Thor's starting X position
-# initial_ty: Thor's starting Y position
-light_x, light_y, initial_tx, initial_ty = [int(i) for i in input().split()]
+light_x, light_y, current_tx, current_ty = [int(i) for i in input().split()]
 
 # game loop
 while True:
-    remaining_turns = int(input())  # The remaining amount of turns Thor can move. Do not remove this line.
-
-    delta_y = light_y - initial_ty
-    delta_x = light_x - initial_tx
+    remaining_turns = int(input()) 
+    Direction = ''
     
     if remaining_turns > 0:
-        while delta_y!=0 or delta_x!=0:
-            if delta_y>0:
-                D_y = 'S'
-                delta_y-=1
-            elif delta_y<0:
-                D_y = 'N'
-                delta_y+=1
-            else:
-                D_y=''
+        if light_y > current_ty:
+            Direction += 'S'
+            current_ty += 1
+        elif light_y < current_ty:
+            Direction += 'N'
+            current_ty -= 1
 
-            if delta_x>0:
-                D_x = 'E'
-                delta_x-=1
-            elif delta_x<0:
-                D_x = 'W'
-                delta_x+=1
-            else:
-                D_x=''
+        if light_x > current_tx:
+            Direction += 'E'
+            current_tx += 1
+        elif light_x < current_tx:
+            Direction += 'W'
+            current_tx -= 1
 
-            D = D_y + D_x
-            print(D)
+        print(Direction)
